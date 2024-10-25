0f83fc2ada159c5890348da51e78d63f6fc8d7bb
that-fela <henricoder@gmail.com>
2024-10-25 19:27:29 +1300

    cmake restructure

--- src/pis/main_pi.cpp
+++ src/pis/main_pi.cpp
@@ -1,20 +1,25 @@
-#include "fmt/format.h"
+#include "fmt/format.h" // NOLINT
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/property_tree/ptree.hpp>
+#include <nlohmann/json.hpp>
 #include <opencv2/opencv.hpp>
-#include <string>
 
 constexpr std::string_view CONFIG_PATH = "../../pi-setup.json";
 
 int main() {
-    fmt::print("Hello from pi\n");
+    fmt::print("Hello from pi {}\n");
 
-    boost::property_tree::ptree config_json;
-    boost::property_tree::read_json(CONFIG_PATH.data(), config_json);
+    std::ifstream file(CONFIG_PATH.data());
+    if (!file.is_open()) {
+        fmt::print("Failed to open config file: {}\n", CONFIG_PATH);
+        return 1;
+    }
 
-    for (const auto &rpi : config_json.get_child("raspberry-pis")) {
-        fmt::print("Pi {} IP: {}\n", rpi.second.get<std::string>("name"),
-                   rpi.second.get<std::string>("ip"));
+    nlohmann::json data = nlohmann::json::parse(file);
+
+    for (const auto &pi : data["raspberry-pis"]) {
+        std::cout << "Name: " << pi["name"] << ", IP: " << pi["ip"]
+                  << std::endl;
     }
 
     return 0;
